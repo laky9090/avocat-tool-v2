c535ca8efa3800f69386ed578dba2e0b7cc546dc
laky9090 <dhllaky@gmail.com>
2025-03-30 21:58:24 +0200

    Initialisation propre sans les fichiers lourds

---
+++ main.js
@@ -0,0 +1,28 @@
+const { app, BrowserWindow } = require('electron');
+const path = require('path');
+const remoteMain = require('@electron/remote/main');
+
+remoteMain.initialize();
+
+function createWindow() {
+  const win = new BrowserWindow({
+    width: 1000,
+    height: 800,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true
+    }
+  });
+
+  win.loadFile('index.html');
+  remoteMain.enable(win.webContents);
+}
+
+app.whenReady().then(createWindow);
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') app.quit();
+});
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) createWindow();
+});
